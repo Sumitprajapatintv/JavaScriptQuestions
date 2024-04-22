9c09e5638d872c115c505512c636e0fabc6ec2bf
Sumitprajapatintv <sumit963605@gmail.com>
2024-04-22 22:45:59 +0530

    Learn Every Method

---
+++ basic/q7.js
@@ -0,0 +1,1 @@
+//Write a JavaScript program to find out if 1st January will be a Sunday between 2014 and 2050.
---
+++ fundamental/Q1.js
@@ -0,0 +1,15 @@
+//Write a JavaScript program to compare two objects to determine if the first contains equivalent property values to the second one.
+const matches = (obj1, obj2) =>
+  Object.keys(obj2).every(
+    (key) => obj1.hasOwnProperty(key) && obj1[key] == obj2[key]
+  );
+
+console.log(
+  matches({ age: 25, hair: "long", beard: true }, { hair: "long", beard: true })
+); // true
+console.log(
+  matches({ hair: "long", beard: true }, { age: 25, hair: "long", beard: true })
+); // false
+console.log(
+  matches({ hair: "long", beard: true }, { age: 26, hair: "long", beard: true })
+); // false
